ae91ee6770787b50f55dcbd42115e229d2132a93
nikhuff <nik.huff@me.com>
2018-03-02 23:03:34 -0700

    created main file for testing

---
+++ main.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    # main function to sample dungeon generator
+    # this is where we'll store the arrays of
+    # type of rooms and the pre-instanciated rooms
+    pass
